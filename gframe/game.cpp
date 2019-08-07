7edef4dcf5ab2b434b8ce550758cb086a96f88b4
Kaiba Corporation <kaiba.corporation.github@gmail.com>
2019-08-06 20:39:28 -0400

    use standard thread library (#2221)
    * use standard thread library
    * use standard sleep function

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -830,11 +830,7 @@ void Game::MainLoop() {
 		fps++;
 		cur_time = timer->getTime();
 		if(cur_time < fps * 17 - 20)
-#ifdef _WIN32
-			Sleep(20);
-#else
-			usleep(20000);
-#endif
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		if(cur_time >= 1000) {
 			myswprintf(cap, L"Yu-Gi-Oh! The Dawn of a New Era FPS: %d", fps);
 			device->setWindowCaption(cap);
@@ -849,11 +845,7 @@ void Game::MainLoop() {
 	DuelClient::StopClient(true);
 	if(dInfo.isSingleMode)
 		SingleMode::StopPlay(true);
-#ifdef _WIN32
-	Sleep(500);
-#else
-	usleep(500000);
-#endif
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 //	SaveConfig();
 //	device->drop();
 }
