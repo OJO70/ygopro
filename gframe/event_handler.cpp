3e12c4ebb69749bdd0e3010302afd9b77cb2129c
Kaiba Corporation <kaiba.corporation.github@gmail.com>
2020-01-26 13:31:25 -0500

    remove MSG_SORT_CHAIN

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -718,7 +718,6 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 					ShowSelectSum(true);
 					break;
 				}
-				case MSG_SORT_CHAIN:
 				case MSG_SORT_CARD: {
 					int offset = mainGame->scrCardList->getPos() / 10;
 					int sel_seq = id - BUTTON_CARD_0 + offset;
@@ -2423,7 +2422,6 @@ void ClientField::CancelOrFinish() {
 		}
 		break;
 	}
-	case MSG_SORT_CHAIN:
 	case MSG_SORT_CARD: {
 		if(mainGame->wCardSelect->isVisible()) {
 			DuelClient::SetResponseI(-1);
